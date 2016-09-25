a6213c936cb5f4f53d862b8addf6d4c5bf8670a7
Shinsuke Abe <mao.instantlife@gmail.com>
2016-09-25 11:36:34 +0900

    h2oイメージを別に立てる構成に変更する

--- rstudio/run_rstudio.sh
+++ rstudio/run_rstudio.sh
@@ -1,3 +1,3 @@
 # !/bin/sh
 
-docker run --name rstudio -d --env-file=`pwd`/.env.list -v `pwd`:/home/rstudio -p 8787:8787 rocker/rstudio
+docker run --name rstudio -d -v `pwd`:/home/rstudio -p 8787:8787 rocker/rstudio
